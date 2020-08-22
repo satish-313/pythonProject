178a9aae2709d436cdac41e6ee4ae3d4c1974346
satish-313 <pradhansatish53@gmail.com>
2020-08-22 17:07:19 +0530

    branch separate the pc and user

--- guessThePosition.py
+++ guessThePosition.py
@@ -21,6 +21,18 @@ def display(space,s):
         print(" ")
     return space
 
+def coordinateRepeatCheckcomputer(spaceUsed,row,col):
+    for i in spaceUsed:
+        if(row == i.row and col == i.col):
+            rowAgain = random.randint(0,2)
+            colAgain = random.randint(0,2)
+            s = coordinate(rowAgain,colAgain)
+            spaceUsed,s = coordinateRepeatCheckcomputer(spaceUsed,s.row,s.col)
+            return spaceUsed,s
+    s = coordinate(row,col)
+    spaceUsed.append(s)
+    return spaceUsed,s
+    
 def coordinateRepeatCheck(spaceUsed,row,col):
     if (len(spaceUsed) == 0):
         s = coordinate(row,col)
@@ -29,11 +41,9 @@ def coordinateRepeatCheck(spaceUsed,row,col):
     else:
         for i in spaceUsed:
             if(row == i.row and col == i.col):
-                #print(i.row+1 ," and ",i.col+1 ," already used enter another coordinate : ")
-                #rowAgain = int(input("Enter the row : "))
-                rowAgain = random.randint(1,3)
-                #colAgain = int(input("Enter the col : "))
-                colAgain = random.randint(1,3)
+                print(i.row+1 ," and ",i.col+1 ," already used enter another coordinate : ")
+                rowAgain = int(input("Enter the row : "))
+                colAgain = int(input("Enter the col : "))
                 s = coordinate(rowAgain-1,colAgain-1)
                 spaceUsed,s = coordinateRepeatCheck(spaceUsed,s.row,s.col)
                 return spaceUsed,s
@@ -44,20 +54,30 @@ def coordinateRepeatCheck(spaceUsed,row,col):
 def main():
     diagonalOfSpace = int(input("Enter matrix diagonal : "))
     space = [["*" for i in range(diagonalOfSpace)]for j in range(diagonalOfSpace)]
+    print("The number attempt is equal to diagonal of matrix that is :",diagonalOfSpace)
     spaceUsed = []
-    i = 0
-    while i != 5:
-        #row = int(input("Enter row index : "))
-        row = random.randint(1,3)
-        row = row - 1
-        #col = int(input("Enter the column index : "))
-        col = random.randint(1,3)
-        col = col - 1
-        spaceUsed,s = coordinateRepeatCheck(spaceUsed, row,col)
-        space = display(space,s)
-        print(i+1," round end !! ")
-        i  = i + 1
-    
+    while True:
+        for i in range(diagonalOfSpace): # for user for input
+            row = int(input("Enter row index : "))
+            row = row - 1
+            col = int(input("Enter the column index : "))
+            col = col - 1
+            spaceUsed,s = coordinateRepeatCheck(spaceUsed,row,col)
+            space = display(space,s)
+            print("The ",(i+1)," position is filled ")
+
+        print("The computer term start")
+        for i in range(diagonalOfSpace): # for computer input by random
+            row = random.randint(0,2)
+            col = random.randint(0,2)
+            spaceUsed,s = coordinateRepeatCheckcomputer(spaceUsed, row,col)
+            space = display(space,s)
+            print("The ",(i+1)," position is filled ")
+        
+        break
+    print("### GAME START ###")
+    print("Who guess the opponent position find first will win\nENJOY!")
+
     print("!! END !!")
     
 
